40521fb7e351aa9b63c8f43f983198574d536eb0
Thomega35 <thomega35@gmail.com>
2024-12-02 19:01:18 +0100

    Add J2 solution in go

---
+++ 2024/j2_1/j2.go
@@ -0,0 +1,19 @@
+// open j1.txt
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	f, inputParsed := intOpenFile("input.txt")
+	numberOfSafeReports := 0
+	for _, report := range inputParsed {
+		if safeUp(report) || safeDown(report) {
+			numberOfSafeReports++
+		}
+	}
+	fmt.Printf("There are %d safe reports\n", numberOfSafeReports)
+
+	defer f.Close()
+}
