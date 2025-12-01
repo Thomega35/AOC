9d8c580128f2954cd5faa009ed3d8554de79eb7c
Thomega35 <thomega35@gmail.com>
2025-12-01 21:03:00 +0100

    j1 files

--- 2025/j1/part_1/solution.py
+++ 2025/j1/part_1/solution.py
@@ -1,11 +1,31 @@
 import utils
+class Direction :
+    LEFT = 'L'
+    RIGHT = 'R'
 
 def solution(vals_parsed) :
-    res = utils.todo(vals_parsed)
+    dial = 50
+    res = 0
+    for line in vals_parsed :
+        line = line[0]
+        direction = Direction.LEFT if line[0] == 'L' else Direction.RIGHT
+        steps = int(line[1:])
+        dial = get_new_pos(dial, direction, steps)
+        if dial == 0 :
+            res += 1
+    return res
+
+def get_new_pos(start : int, direction : Direction, steps : int) -> int:
+    res = start
+    if direction == Direction.LEFT :
+        res -= steps
+        res = res % 100
+    elif direction == Direction.RIGHT :
+        res += steps
+        res = res % 100
     return res
 
 if __name__ == "__main__" :
     vals = open('input.txt', 'r').read().splitlines()
-    vals_parsed = list(vals[0].split(','))
-    print(vals_parsed)
+    vals_parsed = [ i.split(',') for i in (vals)]
     print(solution(vals_parsed))
--- 2025/j1/part_2/solution.py
+++ 2025/j1/part_2/solution.py
@@ -1,11 +1,36 @@
 import utils
+class Direction :
+    LEFT = 'L'
+    RIGHT = 'R'
 
 def solution(vals_parsed) :
-    res = utils.todo(vals_parsed)
+    dial = 50
+    res = 0
+    for line in vals_parsed :
+        line = line[0]
+        direction = Direction.LEFT if line[0] == 'L' else Direction.RIGHT
+        steps = int(line[1:])
+        dial, bonus = get_new_pos(dial, direction, steps)
+        res += bonus
+        if dial == 0 :
+            res += 1
     return res
+
+def get_new_pos(start : int, direction : Direction, steps : int) -> tuple[int, int] :
+    res = start
+    bonus = abs(steps - (steps % 100)) // 100
+    steps = steps % 100
+    if direction == Direction.LEFT :
+        res = (res - steps) % 100
+        if start != 0 and res != 0 :
+            bonus += 1 if res != start - steps else 0
+    elif direction == Direction.RIGHT :
+        res = (res + steps) % 100
+        if start != 0 and res != 0 :
+            bonus += 1 if res != start + steps else 0
+    return res, bonus
 
 if __name__ == "__main__" :
     vals = open('input.txt', 'r').read().splitlines()
-    vals_parsed = list(vals[0].split(','))
-    print(vals_parsed)
+    vals_parsed = [ i.split(',') for i in (vals)]
     print(solution(vals_parsed))
